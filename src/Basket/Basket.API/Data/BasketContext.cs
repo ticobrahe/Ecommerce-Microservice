c7fac7a8408bd75cef44da22d26c8586cee63511
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-07-31 11:47:43 +0100

    implement basket service

---
+++ src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Basket.API.Data.Interface;
+using StackExchange.Redis;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+        public IDatabase Redis { get; }
+
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = redisConnection.GetDatabase();
+        }
+    }
+}
